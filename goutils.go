0cfe32caa9d4e8bac1f0c295e505195b379a1262
Gulomjon Kamolov <g.kamolov@gmail.com>
2022-08-17 16:16:13 +0500

    update

--- goutils.go
+++ goutils.go
@@ -1,9 +1,26 @@
 package goutils
 
-func getmax(a int, b int) int {
+// Function to get max of two integers
+func Getmax(a int, b int) int {
 	if a > b {
 		return a
 	} else {
 		return b
+	}
+}
+
+<<<<<<< HEAD
+// Function to convert digits into uzbek words
+=======
+//  this is another commit
+
+>>>>>>> 8a32f792edd164ee7db6c7d152a762d28fde6538
+func NumberToWord(a int) string {
+	var numbers = []string{"nol", "bir", "ikki", "uch", "tort", "besh", "olti", "yetti", "sakkiz", "tuqqiz"}
+
+	if a >= 0 && a < 10 {
+		return numbers[a]
+	} else {
+		return "Bu son 1 va 10 oralig'ida emas!"
 	}
 }
