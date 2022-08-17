34cc352afe6f8777860ab5a19dd6e80a11fcedf4
Gulomjon Kamolov <g.kamolov@gmail.com>
2022-08-17 17:17:36 +0500

    test update

--- goutils.go
+++ goutils.go
@@ -1,7 +1,7 @@
 package goutils
 
 func NumberToWord(a int) string {
-	var numbers = []string{"нол", "бир", "икки", "уч", "тўрт", "беш", "олти", "етти", "саккиз", "тўққиз"}
+	var numbers = []string{"нол", "бир", "икки", "уч бурчак", "тўрт", "беш", "олти", "етти", "саккиз", "тўққиз"}
 
 	if a >= 0 && a < 10 {
 		return numbers[a]
